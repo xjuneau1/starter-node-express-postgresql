7f3ee9e1523f88a4e002e97129d9f64ecc7cb77c
Xander Juneau <xanderjuneau@gmail.com>
2022-09-12 17:52:48 -0400

    added migrations, executed and set up DB, added seed files

---
+++ knexfile.js
@@ -0,0 +1,23 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+ const path = require("path")
+ require("dotenv").config()
+ const {DATABASE_URL} = process.env
+ 
+ module.exports = {
+ 
+   development: {
+     client: 'postgresql',
+     connection: DATABASE_URL,
+     migrations: {
+       directory: path.join(__dirname, "src", "db", "migrations")
+     },
+     seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds")
+     }
+   }
+ };
+ 
